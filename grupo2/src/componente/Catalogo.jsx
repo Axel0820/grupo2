176390afb7a34f911c676e803d9acdcff6e956f0
Tu Nombre <tu@email.com>
2025-11-03 18:58:18 -0300

    Update App.js and package-lock.json

---
+++ grupo2/src/componente/Catalogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Catalogo = () => {
+    return (
+        <div>
+            <h1>Catálogo de Productos</h1>
+            {/* Aquí puedes agregar los componentes del catálogo */}
+        </div>
+    );
+}
+
+export default Catalogo;
